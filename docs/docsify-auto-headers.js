5718f7bc539a8f2b967a0b74d5851fb08dc85fd8
Mark Battistella <mark_battistella+github@hotmail.com>
2024-06-02 11:16:25 +1000

    .

--- docs/docsify-auto-headers.js
+++ docs/docsify-auto-headers.js
@@ -1,49 +1,46 @@
-'use strict';
+(function () {
 
-const docsifyAutoHeaders = {
-    separator: '.',
+    'use strict';
 
-    levels: 6,
-    // levels: { start: 2, finish: 3 },
+    const docsifyAutoHeadersDefaults = {
+        separator: '.',
 
-    // scope: '#main',
-    scope: { element: '#main', sidebar: true },
-    // scope: { element: '#main', sidebar: false },
+        levels: 6,
+        // levels: { start: 2, finish: 3 },
 
-    debug: true
-};
+        sidebar: true,
+        // sidebar: false,
 
+        debug: true
+    };
 
-const autoHeaders = (hook, vm) => {
     const defaultErrors = {
         configurationNotSetCorrectly:
-            'ERROR: config settings not set',
+            'Config settings not set',
         invalidHeadingLevels:
-            'ERROR: the levels parameter needs to be either a number (1-6) or an object',
+            'The levels parameter needs to be either a number (1-6) or an object',
         invalidHeadingLevelNumber:
-            'ERROR: heading levels need to be between 1-6',
+            'Heading levels need to be between 1-6',
         invalidHeadingLevelOrder:
-            'ERROR: heading start level cannot be greater than finish level',
+            'Heading start level cannot be greater than finish level',
         invalidHeadingLevelRange:
-            'ERROR: heading levels need to be a value of 1 through to 6',
+            'Heading levels need to be a value of 1 through to 6',
         nonNumericValue:
-            'ERROR: the levels start or finish need to be numeric',
+            'The levels start or finish need to be numeric',
         mismatchedSeparator:
-            'ERROR: the config separator does not match the signifier separator',
+            'The config separator does not match the signifier separator',
         missingAutoHeaderSignifier:
-            'ERROR: the markdown file is missing the @autoHeader: or <!-- autoHeader: --> signifier',
+            'The markdown file is missing the @autoHeader: or <!-- autoHeader: --> signifier',
         misconfiguredSignifier:
-            'ERROR: the markdown file has the signifier, but it is misconfigured',
-        misconfiguredScope:
-            'ERROR: the scope has been misconfigured. It must be a string (an element, ID, or class name), or an object',
-        scopeNotFound:
-            'ERROR: the specified scope was not found in the document'
+            'The markdown file has the signifier, but it is misconfigured',
+        invalidSidebar:
+            'The sidebar parameter needs to be a boolean - true or false'
     };
 
     const setDefaultOptions = (options) => {
         if (!options.separator || options.levels === undefined) {
-            console.warn(defaultErrors.configurationNotSetCorrectly);
-            return;
+            console.info(defaultErrors.configurationNotSetCorrectly);
+            throw new Error(defaultErrors.configurationNotSetCorrectly);
         }
 
         const separatorMap = {
@@ -56,33 +53,23 @@ const autoHeaders = (hook, vm) => {
         };
         const separator = separatorMap[options.separator] || options.separator;
         const levels = options.levels || 6;
-        const scope = options.scope || '#main';
+        const sidebar = !!options.sidebar;
         const debug = !!options.debug;
 
-        return { separator, levels, scope, debug };
+        return { separator, levels, sidebar, debug };
     };
 
     const getUsingSidebar = (input) => {
-        if (typeof input !== 'string' && (typeof input !== 'object' || input === null)) {
-            console.error(defaultErrors.misconfiguredScope);
-            return;
-        }
-
-        if (typeof input === 'string') {
-            return { element: input, sidebar: false };
-        } else if (typeof input === 'object') {
-            if (typeof input.sidebar !== 'boolean') {
-                console.warn(defaultErrors.misconfiguredScope);
-                return;
-            }
-            return { element: input.element, sidebar: input.sidebar };
+        if (typeof input !== 'boolean') {
+            throw new Error(defaultErrors.invalidSidebar);
+        } else {
+            return input;
         }
     };
 
     const getHeadingRange = (input) => {
         if (typeof input !== 'number' && (typeof input !== 'object' || input === null)) {
-            console.error(defaultErrors.invalidHeadingLevels);
-            return;
+            throw new Error(defaultErrors.invalidHeadingLevels);
         }
 
         const isInRange = (value, min, max) => value >= min && value <= max;
@@ -96,19 +83,16 @@ const autoHeaders = (hook, vm) => {
             ({ start, finish } = input);
 
             if (typeof start !== 'number' || typeof finish !== 'number') {
-                console.error(defaultErrors.nonNumericValue);
-                return;
+                throw new Error(defaultErrors.nonNumericValue);
             }
 
             if (start > finish) {
-                console.warn(defaultErrors.invalidHeadingLevelOrder);
-                return;
+                throw new Error(defaultErrors.invalidHeadingLevelOrder);
             }
         }
 
         if (!isInRange(start, 1, 6) || !isInRange(finish, 1, 6)) {
-            console.warn(defaultErrors.headingLevelRange);
-            return;
+            throw new Error(defaultErrors.headingLevelRange);
         }
 
         const headings = {};
@@ -128,8 +112,7 @@ const autoHeaders = (hook, vm) => {
         const isAlphabetic = elements.every(isAllAlphabetic);
 
         if (!(isNumeric || isAlphabetic)) {
-            console.error(defaultErrors.invalidHeadingLevels, headerNumbers);
-            return null;
+            throw new Error(defaultErrors.invalidHeadingLevels);
         }
 
         while (elements.length < 6) {
@@ -150,6 +133,7 @@ const autoHeaders = (hook, vm) => {
         return match ? match[1] : null;
     };
 
+
     const createCountContextObjects = (levels) => {
         const configEntries = Object.entries(levels);
         const currentCounts = new Map(
@@ -167,12 +151,12 @@ const autoHeaders = (hook, vm) => {
             scopedTagNames,
         };
     };
-    
+
     const applyCurrentCountThroughBoundContext = function (headingNode, options) {
         const { currentCounts, scopedTagNames } = this;
         const headingName = headingNode.tagName.toLowerCase();
         const headingNameList = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
-        
+
         // Reset the counts for headings that are below the current heading level
         headingNameList.slice(headingNameList.indexOf(headingName) + 1).forEach(name => {
             if (currentCounts.has(name)) {
@@ -181,11 +165,11 @@ const autoHeaders = (hook, vm) => {
                 nextMinorCounts.reset = 0;
             }
         });
-    
+
         // Update the count for the current heading
         const counts = currentCounts.get(headingName);
         counts.current += 1;
-    
+
         if (scopedTagNames.has(headingName)) {
             const counterValue = headingNameList
                 .slice(0, headingNameList.indexOf(headingName) + 1)
@@ -193,20 +177,20 @@ const autoHeaders = (hook, vm) => {
                     counterList.concat(currentCounts.get(name).current), []
                 )
                 .join(options.separator) + options.separator;
-    
+
             headingNode.innerHTML = `${counterValue} ${headingNode.innerHTML}`;
         }
     };
-        
+
     const parseMarkdown = (markdown, options, levels) => {
         const { currentCounts, scopedTagNames } = createCountContextObjects(levels);
         const headingPattern = /^(#{1,6})\s+(.*)$/gm;
         const headingNameList = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
-    
+
         let result = markdown.replace(headingPattern, (match, hashes, text) => {
             const headingLevel = hashes.length;
             const headingName = `h${headingLevel}`;
-            
+
             // Reset the counts for headings that are below the current heading level
             headingNameList.slice(headingLevel).forEach(name => {
                 if (currentCounts.has(name)) {
@@ -215,11 +199,11 @@ const autoHeaders = (hook, vm) => {
                     nextMinorCounts.reset = 0;
                 }
             });
-    
+
             // Update the count for the current heading
             const counts = currentCounts.get(headingName);
             counts.current += 1;
-    
+
             if (scopedTagNames.has(headingName)) {
                 const counter = Array.from({ length: headingLevel }, (_, i) => {
                     return currentCounts.get(`h${i + 1}`).current;
@@ -229,16 +213,15 @@ const autoHeaders = (hook, vm) => {
                 return match;
             }
         });
-    
+
         return result;
     };
-    
+
     const applyScopedHeadingCounts = (levels, options, input, type) => {
         const { currentCounts, scopedTagNames } = createCountContextObjects(levels);
 
         if (type === 'html') {
-            const scope = document.querySelector(options.scope.element);
-            const headingList = [...scope.querySelectorAll('h1, h2, h3, h4, h5, h6')];
+            const headingList = [...input.querySelectorAll('h1, h2, h3, h4, h5, h6')];
             headingList.forEach(
                 heading => applyCurrentCountThroughBoundContext.call(
                     { currentCounts, scopedTagNames },
@@ -252,84 +235,101 @@ const autoHeaders = (hook, vm) => {
         }
     };
 
+    const autoHeaders = (hook, vm) => {
 
-    // Setup default options and scope
-    const defaultOptions = setDefaultOptions(docsifyAutoHeaders);
-    let options = {
-        separator: defaultOptions.separator,
-        levels: getHeadingRange(defaultOptions.levels),
-        scope: getUsingSidebar(defaultOptions.scope),
-        debug: defaultOptions.debug
+        // Setup default options and scope
+        const defaultOptions = setDefaultOptions(docsifyAutoHeadersDefaults);
+        let options = {
+            separator: defaultOptions.separator,
+            levels: getHeadingRange(defaultOptions.levels),
+            sidebar: getUsingSidebar(defaultOptions.scope),
+            debug: defaultOptions.debug
+        };
+
+        // MARK: Need to access markdown to extract the signifier data
+        hook.beforeEach((markdown, next) => {
+            try {
+                const headingSignifier = checkAutoHeader(markdown);
+                if (!headingSignifier) {
+                    throw new Error(defaultErrors.missingAutoHeaderSignifier);
+                }
+
+                const headingRanges = getHeadingRange(defaultOptions.levels);
+                const startingHeadingValues = getStartingValues(
+                    headingSignifier,
+                    options.separator
+                );
+                if (!startingHeadingValues) {
+                    throw new Error(defaultErrors.misconfiguredSignifier);
+                }
+                const headingConfiguration = {};
+                for (const key in headingRanges) {
+                    headingConfiguration[key] = {
+                        ...headingRanges[key],
+                        ...startingHeadingValues[key]
+                    };
+                }
+
+                // Update the options for use elsewhere
+                options.levels = headingConfiguration;
+
+                const cleanedMarkdown = markdown.split('\n').slice(1).join('\n');
+                return cleanedMarkdown;
+
+            } catch (error) {
+                console.warn('Warning: Docsify Auto Headers\n', error.message);
+            } finally {
+                next(markdown);
+            }
+        });
+
+        // Conditional setup for hooks
+        if (options.sidebar) {
+
+
+            hook.beforeEach((markdown, next) => {
+                let output;
+                try {
+                    output = applyScopedHeadingCounts(
+                        options.levels,
+                        options,
+                        markdown,
+                        'markdown'
+                    );
+                } catch (error) {
+                    console.warn('Warning: Docsify Auto Headers\n', error.message)
+                } finally {
+                    next(output);
+                }
+            });
+        } else {
+            hook.afterEach(function (html, next) {
+                let output;
+                try {
+                    output = applyScopedHeadingCounts(
+                        options.levels,
+                        options,
+                        html,
+                        'html'
+                    );
+                } catch (error) {
+                    console.warn('Warning: Docsify Auto Headers\n', error.message)
+                } finally {
+                    next(output);
+                }
+            });
+        }
     };
 
-    // MARK: Need to access markdown to extract the signifier data
-    hook.beforeEach(markdown => {
-        const headingSignifier = checkAutoHeader(markdown);
-        if (!headingSignifier) {
-            console.warn(defaultErrors.missingAutoHeaderSignifier);
-            return markdown;
-        }
+    // Merge user configuration with default configuration
+    window.$docsify = window.$docsify || {};
+    window.$docsify.autoHeaders = Object.assign(
+        docsifyAutoHeadersDefaults,
+        window.$docsify.autoHeaders
+    );
 
-        const headingRanges = getHeadingRange(defaultOptions.levels);
-        const startingHeadingValues = getStartingValues(
-            headingSignifier,
-            options.separator
-        );
-        if (!startingHeadingValues) {
-            console.warn(defaultErrors.misconfiguredSignifier);
-            return markdown;
-        }
-        const headingConfiguration = {};
-        for (const key in headingRanges) {
-            headingConfiguration[key] = {
-                ...headingRanges[key],
-                ...startingHeadingValues[key]
-            };
-        }
-
-        // Update the options for use elsewhere
-        options.levels = headingConfiguration;
-
-        const cleanedMarkdown = markdown.split('\n').slice(1).join('\n');
-        return cleanedMarkdown;
-    });
-
-
-    // Conditional setup for hooks
-    if (options.scope.sidebar) {
-        hook.beforeEach(markdown => {
-            return applyScopedHeadingCounts(
-                options.levels,
-                options,
-                markdown,
-                'markdown'
-            );
-        });
-    } else {
-        hook.doneEach(() => {
-            const scopeElement = document.querySelector(options.scope.element);
-            if (!scopeElement) {
-                console.warn(defaultErrors.scopeNotFound);
-                return;
-            }
-
-            return applyScopedHeadingCounts(
-                options.levels,
-                options,
-                null,
-                'html'
-            );
-        });
-    }
-};
-
-
-// find heading plugin options
-window.$docsify.autoHeaders = Object.assign(
-    docsifyAutoHeaders,
-    window.$docsify.autoHeaders
-);
-window.$docsify.plugins = [].concat(
-    autoHeaders,
-    window.$docsify.plugins
-);
+    // Add plugin to docsify's plugin array
+    window.$docsify.plugins = (
+        window.$docsify.plugins || []
+    ).concat(autoHeaders);
+})();
